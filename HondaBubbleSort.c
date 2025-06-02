00f6071b88319c409ecff6410c86fec22cb17f07
Davi Faria <djofaria.cic@uesc.br>
2025-06-02 07:51:39 -0300

    Update HondaBubbleSort.c

--- HondaBubbleSort.c
+++ HondaBubbleSort.c
@@ -78,7 +78,6 @@ printf("\n\n");
 //histograma
 printf("==============Histograma==============\n");
 
-/*
 printf("Zero:   |");
 for (int i = 0; i < 20; i++){
     if(notas[i] == 0){
@@ -120,7 +119,7 @@ for (int i = 0; i < 20; i++){
         printf("=");
     }
 }
-printf("\n");*/
+printf("\n");
 
 return 0;
 }
